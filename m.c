24ea6a4cb6bbb10781dd9ba3d0c2cd0e1abf93ad
raghuram <raghuram.ram84@gmail.com>
2022-07-08 00:01:51 +0530

    Tested by Raghuram

--- m.c
+++ m.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 
+#define fun(a,b) a+b
 typedef enum{mon,tue,wed}data;
 
 
@@ -8,7 +9,7 @@ int main()
 
 data d=tue;
 
-printf("raghuram said %d \n",d);
+printf("raghuram said its test only result= %d %d\n",d,fun(1,2));
 
 
 return 0;
